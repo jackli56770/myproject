44a9279c23ee1790766d37c9b387141bb1da3039
tf00562770 <562770@gmail.com>
2019-09-18 21:00:36 +0800

    Initial commit

---
+++ src/main/kotlin/com/jack/Hello.kt
@@ -0,0 +1,12 @@
+package com.jack
+
+fun main() {
+//    println("Hello World! by KT");
+    val theHuman = Human().Hello();
+}
+
+class Human {
+    fun Hello() {
+        println("Hello World! by KT");
+    }
+}
