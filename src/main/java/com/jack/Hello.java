4fc06756334d0ee06a9793f3d10ffa7844a21d59
tf00562770 <562770@gmail.com>
2019-09-18 21:20:39 +0800

    version2 java kotlin variable, datatype

--- src/main/java/com/jack/Hello.java
+++ src/main/java/com/jack/Hello.java
@@ -5,5 +5,14 @@ public class Hello {
 //        System.out.println("Hello World!");
         Person thePerson = new Person();
         thePerson.hello();
+        int age = 19;
+        Integer age2 = 19;
+        char c =  'a';
+        byte b = 120;
+        float weight = 66.5f;
+        double height = 1.7;
+        boolean isAdult = true;
+        boolean isEnroll = false;
+        String strName = "test";
     }
 }
--- src/main/kotlin/com/jack/Hello.kt
+++ src/main/kotlin/com/jack/Hello.kt
@@ -1,8 +1,15 @@
 package com.jack
 
+import javax.print.DocFlavor
+
 fun main() {
 //    println("Hello World! by KT");
-    val theHuman = Human().Hello();
+    val theHuman = Human();
+    theHuman.Hello();
+    val age : Int = 19;
+    var weigth  = 66.5f;
+    val name = "jack";
+
 }
 
 class Human {
